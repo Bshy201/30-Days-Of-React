c574026d41e8f9e8186812c09319147405161826
bshy96@gmail.com <bshy96@gmail.com>
2024-09-25 11:27:12 -0400

    Finished Level2 & level3

--- solutions/day-01/main.js
+++ solutions/day-01/main.js
@@ -36,3 +36,40 @@ const fullStack = [frontEnd, backEnd]
 
 console.log(fullStack)
   
+
+//Level 3
+//1. 
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+console.log(ages.sort())
+let len = ages.length
+const min = ages[0]
+const max = ages[len -1]
+const middle = ages[len / 2] 
+const median = middle / 2
+let sum = 0
+ages.forEach(int => {
+  sum += int
+})
+const average = sum / len
+const range = max - min
+const value1 = min - average
+const value2 = max - average
+const difference = Math.abs(value1, value2)
+console.log("Min: ", min)
+console.log("Max: ", max)
+console.log("Median: ", median)
+console.log("Average: ", average)
+console.log("Range", range)
+console.log("Difference", difference)
+
+//2.
+const countryLen = countries.length
+const middleCountryLen = parseInt(countryLen / 2)
+const middleCountry = (countries[parseInt(countryLen / 2)])
+console.log( middleCountry) 
+
+//3.
+let arr1 = countries.slice(0, middleCountryLen)
+let arr2 = countries.slice(middleCountryLen, middleCountryLen + countryLen)
+
+console.log('Array1: ' + arr1 + '\nArray2: ' + arr2)
